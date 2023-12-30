b03f8824640e4d22fbe230890781f6e8c07e3feb
wmzn-ltpp <1491579574@qq.com>
2023-12-30 19:03:40 +0800

    feat:update

--- web_build.sh
+++ web_build.sh
@@ -3,14 +3,14 @@
  # @Author: 18855190718 1491579574@qq.com
  # @Date: 2023-08-22 13:31:27
  # @LastEditors: wmzn-ltpp 1491579574@qq.com
- # @LastEditTime: 2023-10-15 12:15:54
+ # @LastEditTime: 2023-12-30 19:03:34
  # @FilePath: \LTPP-CODE\web_build.sh
  # @Description: Email:1491579574@qq.com
  # QQ:1491579574
  # Copyright (c) 2023 by SQS, All Rights Reserved. 
 ###
 cd Frontend
-cnpm run build
-cnpm run build
+npm run build
+npm run build
 cd ../
 ./web_up.sh
