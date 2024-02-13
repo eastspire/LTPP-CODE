baacd7ef0e0f98f3d3c791279850ba8ed73a495f
wmzn-ltpp <1491579574@qq.com>
2024-02-13 10:55:49 +0800

    feat:update

--- config/plugin/webman/console/app.php
+++ config/plugin/webman/console/app.php
@@ -9,16 +9,7 @@
  * QQ:1491579574
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
  */
-/*
- * @Author: 18855190718 1491579574@qq.com
- * @Date: 2023-02-28 22:41:18
- * @LastEditors: 18855190718 1491579574@qq.com
- * @LastEditTime: 2023-03-06 23:01:18
- * @FilePath: \API\config\plugin\webman\console\app.php
- * @Description: Email:1491579574@qq.com
- * QQ:1491579574
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
- */
+
 return [
     'enable' => true,
     'phar_file_output_dir' => BASE_PATH . DIRECTORY_SEPARATOR . 'build',
