65dca3b553323853575310360e6a8cb079da3d47
wmzn-ltpp <1491579574@qq.com>
2024-02-22 23:18:32 +0800

    feat:update

--- app/functions.php
+++ app/functions.php
@@ -1,18 +1,5 @@
 <?php
-/*
- * @Author: 18855190718 1491579574@qq.com
- * @Date: 2023-02-28 22:41:18
- * @LastEditors: 18855190718 1491579574@qq.com
- * @LastEditTime: 2023-04-17 22:29:04
- * @FilePath: \LTPP\app\functions.php
- * @Description: Email:1491579574@qq.com
- * QQ:1491579574
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
- */
 // 运行内存限制
-ini_set('memory_limit', '102400M');
+ini_set('memory_limit', '32768M');
 // gzip
 ob_start("ob_gzhandler");
-/**
- * Here is your custom functions.
- */
