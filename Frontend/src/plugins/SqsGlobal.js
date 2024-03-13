01cc95c58a0fc81f5c6824d17a8d271ed4f33ec4
wmzn-ltpp <1491579574@qq.com>
2024-03-13 08:20:28 +0800

    feat:update

--- Frontend/src/plugins/SqsGlobal.js
+++ Frontend/src/plugins/SqsGlobal.js
@@ -8,7 +8,7 @@
  * QQ:1491579574
  * Copyright (c) 2023 by SQS, All Rights Reserved. 
  */
-
+/* 全局变量 */
 import c_tips from './code/c';
 import cpp_tips from './code/cpp';
 import java_tips from './code/java';
@@ -21,12 +21,12 @@ import ruby_tips from './code/ruby';
 import csharp_tips from './code/csharp';
 import python_tips from './code/python';
 
-/* 全局变量 */
+
 const cpp = `#include<bits/stdc++.h>\n#pragma GCC optimize(2)\n#pragma GCC optimize(3)\n#define PF first\n#define PS second\n#define UM unordered_map\n#define US unordered_set\n#define MS mutiset\n#define F(i, start, end, offset) for(int i = start; i < end; i += offset)\n#define F_(i, start, end, offset) for(int i = start; i >= end; i-= offset)\n#define fastcpp ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)\n#define random(a, b) (rand() % (b - a) + a)\nusing namespace std;\nusing LL = long long int;\nusing PII = pair<int, int>;\nusing STR = string;\n\nint main(){\n    fastcpp;\n    \n    \n    return 0;\n}`;
 const c = `#include<stdio.h>\n#pragma GCC optimize(2)\n#pragma GCC optimize(3)\n#define F(i, start, end, offset) for(int i = start; i < end; i += offset)\n#define F_(i, start, end, offset) for(int i = start; i >= end; i-= offset)\n#define random(a, b) (rand() % (b - a) + a)\ntypedef long long int LL;\n\nint main(){\n    \n    \n    return 0;\n}`;
-const java = `//请勿更改类名，否则运行失败!\n\npublic class Main{\n\n    public static void main(String[] args){\n        \n        \n    }\n}`;
+const java = `public class Main{\n    //请勿更改类名，否则运行失败!\n    public static void main(String[] args){\n        \n        \n    }\n}`;
 const python = `def main():\n    res = ''\n    \n    \n    return res\n\nprint(main())`;
-const go = `//请勿更改包名，否则运行失败!\n\npackage main\n\nimport (\n    "fmt"\n    "bufio"\n    "os"\n    "strconv"\n    "strings"\n)\n\nfunc read() []int {\n    var arr = make([]int, 0);\n    inputs := bufio.NewScanner(os.Stdin)\n    for inputs.Scan() {\n        data := strings.Split(inputs.Text(), " ")\n        for i := range data {\n            val, _ := strconv.Atoi(data[i])\n            arr = append(arr, val)\n        }\n    }\n    return arr;\n}\n\nfunc print(arr []int){\n    for i := 0; i < len(arr); i++ {\n        fmt.Print(arr[i]," ")\n    }\n}\n\nfunc main(){\n    var arr = read()\n    \n    \n    print(arr)\n}`;
+const go = `package main\n//请勿更改包名，否则运行失败!\nimport (\n    "fmt"\n    "bufio"\n    "os"\n    "strconv"\n    "strings"\n)\n\nfunc read() []int {\n    var arr = make([]int, 0);\n    inputs := bufio.NewScanner(os.Stdin)\n    for inputs.Scan() {\n        data := strings.Split(inputs.Text(), " ")\n        for i := range data {\n            val, _ := strconv.Atoi(data[i])\n            arr = append(arr, val)\n        }\n    }\n    return arr;\n}\n\nfunc print(arr []int){\n    for i := 0; i < len(arr); i++ {\n        fmt.Print(arr[i]," ")\n    }\n}\n\nfunc main(){\n    var arr = read()\n    \n    \n    print(arr)\n}`;
 const php = `<?php\n\n/**\n * 输入\n * @param [fn] function(cin_line = '', cin_all = []) 每行读取结束后的回调函数(cin_line => 读取的当前行, cin_all => 所有读入)\n * @param [$is_slpit] 是否需要对读入的字符串切割\n * @param [$split_char] 按照什么字符切割\n * @return array\n */\nfunction cin($fn = null, $is_slpit = true, $split_char = ' '){\n    $cin_all = [];\n    if(!$fn){\n        $fn = function(){};\n    }\n    while (!feof(STDIN)){\n        $cin_line = [];\n        $line = trim(fgets(STDIN));\n        if(empty($line)){\n            continue;\n        }\n        $cin_line = $is_slpit ? explode($split_char, $line) : $line;\n        $cin_all[] = $cin_line;\n        $fn($cin_line, $cin_all);\n    }\n    return $cin_all;\n}\n\n(function (){\n    $res = null;\n    $arr = cin(function($read_line = [], $out = []){\n        // 每行读取完成的回调函数\n        \n    }, true);\n    \n    var_dump($res);\n})();`;
 const javascript = `/**\n * 输入\n * @param [fn] Function(cin_line = '', cin_all = []) 每行读取结束后的回调函数(cin_line => 读取的当前行, cin_all => 所有读入)\n * @param [is_split] Boolean 是否需要对读入的字符串切割\n * @param [split_char] String 按照什么字符切割\n * @return void\n */\nasync function cinLine(fn = () => {}, is_split = true, split_char = ' '){\n    const cin_all = [];\n    const readline = require('readline');\n    const rl = readline.createInterface({\n        input: process.stdin,\n        output: process.stdout\n    });\n    return new Promise((resolve, reject) => {\n        try{\n            rl.on('line', function(line){\n                const cin_line = is_split ? line.split(split_char) : line;\n                cin_all.push(cin_line);\n                fn.call(this, cin_line, cin_all);\n            });\n            rl.on('close',() => {\n                resolve();\n            });\n        }catch(e){\n            reject(e);\n        }\n    });\n}\n\n(async() => {\n    let res = null;\n    \n    await cinLine((cin_line = '', cin_all = []) => {\n        // 每行读取完成的回调函数\n        \n    });\n\n    console.log(res);\n})();`;
 const rust = `#![allow(warnings)]\nuse std::io;\nuse std::error::Error;\nuse std::boxed::Box;\nuse std::convert::TryInto;\nuse std::cmp::Ordering;\nuse std::cmp::min;\nuse std::cmp::max;\n\nfn getAns() -> i64 {\n    let res: i64 = 0;\n    \n    \n    return res;\n}\n\nfn main() -> Result<(), Box<dyn Error>> {\n    \n    \n    print!("{}", getAns());\n    Ok(())\n}`;
@@ -37,6 +37,18 @@ const websocket_connect_str = '@ltpp@';
 const loading_tips = '加载中';
 const code_light_css = '--mtk1: #7c4dff !important;--mtk5: #3dc9b0 !important;--mtk6: #fa278e !important;--mtk7: #437aed !important;--mtk8: #d365e5 !important;--mtk9: #437aed !important;--mtk20: #3dc9b0 !important;--mtk23: #3dc9b0 !important;';
 const code_dark_css = '--mtk1: #d365e5 !important;--mtk5: #3dc9b0 !important;--mtk6: #ff9070 !important;--mtk7: #00bec4 !important;--mtk8: #fa278e !important;--mtk9: #E6A23C !important;--mtk20: #3dc9b0 !important;--mtk23: #d365e5 !important;';
+
+c_tips.key.unshift(c);
+cpp_tips.key.unshift(cpp);
+rust_tips.key.unshift(rust);
+javascript_tips.key.unshift(javascript);
+typescript_tips.key.unshift(typescript);
+php_tips.key.unshift(php);
+go_tips.key.unshift(go);
+java_tips.key.unshift(java);
+csharp_tips.key.unshift(csharp);
+ruby_tips.key.unshift(ruby);
+python_tips.key.unshift(python);
 
 // 编辑器主题
 const themelist = [
