8bf1b8a1158c8e31d67d934643c09aaddeac296d
wmzn-ltpp <1491579574@qq.com>
2024-03-29 08:01:07 +0800

    feat:update

--- config/process.php
+++ config/process.php
@@ -56,29 +56,6 @@ return [
         'handler' => process\CleanRobotDb::class,
         'count' => 1
     ],
-    // File update detection and automatic reload
-    // 'monitor' => [
-    //     'handler' => process\Monitor::class,
-    //     'reloadable' => false,
-    //     'constructor' => [
-    //         // Monitor these directories
-    //         'monitor_dir' => [
-    //             app_path(),
-    //             config_path(),
-    //             base_path() . '/process',
-    //             base_path() . '/support',
-    //             base_path() . '/resource',
-    //             base_path() . '/.env',
-    //         ],
-    //         // Files with these suffixes will be monitored
-    //         'monitor_extensions' => [
-    //             'php',
-    //             'html',
-    //             'htm',
-    //             'env'
-    //         ]
-    //     ]
-    // ],
     // 'Register' => [
     //     'handler' => \Webman\App::class,
     //     'listen' => 'http://0.0.0.0:48800',
