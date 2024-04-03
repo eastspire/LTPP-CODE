42f345bed853147603598094dbe07b7ddf1d9540
wmzn-ltpp <1491579574@qq.com>
2024-04-03 21:32:35 +0800

    feat:update

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -23,7 +23,7 @@ export default {
   name: "app",
   data() {
     return {
-      version: "2.2.0",
+      version: "2.3.0",
       get_version_lock: false,
     };
   },
--- app/controller/Version.php
+++ app/controller/Version.php
@@ -20,7 +20,7 @@ class Version
      * 软件版本
      * @var string $version 软件版本
      */
-    static $version = '2.2.0';
+    static $version = '2.3.0';
 
     /**
      * 获取版本
