336c1c51dedb4df8ef497e7c35e92c6db5c16690
尤雨东 <root@ltpp.vip>
2024-06-22 20:50:28 +0800

    feat:update

--- config/plugin/webman/gateway-worker/process.php
+++ config/plugin/webman/gateway-worker/process.php
@@ -18,7 +18,7 @@ return [
     'gateway' => [
         'handler' => Gateway::class,
         'listen' => 'websocket://0.0.0.0:47272',
-        'count' => cpu_count() > 4 ? 4 : cpu_count(),
+        'count' => cpu_count() > 2 ? 2 : cpu_count(),
         'constructor' => [
             'config' => [
                 'lanIp' => '127.0.0.1',
@@ -33,7 +33,7 @@ return [
     ],
     'worker' => [
         'handler' => BusinessWorker::class,
-        'count' => cpu_count() > 4 ? 8 : cpu_count() * 2,
+        'count' => cpu_count() > 4 ? 4 : cpu_count(),
         'constructor' => [
             'config' => [
                 'eventHandler' => plugin\webman\gateway\Events::class,
