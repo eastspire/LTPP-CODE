4ce748068aea457df2a22ae76b98c6609c9c7575
尤雨东 <root@ltpp.vip>
2024-12-21 11:21:08 +0800

    feat:shell

--- sh/push.sh
+++ sh/push.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+git lfs install;
+git config lfs.allowincompletepush true;
 git config --global core.autocrlf input;
-git add .;
-echo -n "Please enter the branch and press Enter:"
-read branch
+echo -n "Please enter the branch and press Enter:";
+read branch;
 if [ -z "$branch" ]; then
-    branch="master"
-fi
-echo -n "Please enter the update message and press Enter:"
-read update_message
-if [ -z "$update_message" ]; then
-    echo "The update message cannot be empty. Please rerun the script and provide a valid update message."
+    echo "The branch cannot be empty.";
     echo "Press Enter to exit...";
     read -n 1;
-    exit 1
+    exit 1;
+fi
+git checkout -b "$branch";
+git push ltpp "$branch";
+git push origin "$branch";
+git add .;
+echo -n "Please enter the update message and press Enter:";
+read update_message;
+if [ -z "$update_message" ]; then
+    echo "The update message cannot be empty. Please rerun the script and provide a valid update message.";
+    echo "Press Enter to exit...";
+    read -n 1;
+    exit 1;
 fi
 git commit -m "feat:$update_message";
 git push github "$branch";
@@ -20,6 +28,15 @@ echo -e "\e[32mgithub push finish\e[0m";
 git push ltpp "$branch";
 echo -e "\e[32mltpp push finish\e[0m";
 git push origin "$branch";
+echo -e "\e[32morigin push finish\e[0m";
+git checkout master;
+git merge "$branch";
+git commit -m "merge:$update_message";
+git push github master;
+echo -e "\e[32mgithub push finish\e[0m";
+git push ltpp master;
+echo -e "\e[32mltpp push finish\e[0m";
+git push origin master;
 echo -e "\e[32morigin push finish\e[0m";
 echo "Press Enter to continue...";
 read -n 1;
