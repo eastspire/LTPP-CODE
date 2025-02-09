75720ca3e96c782f1a3a572dd6379e7f81a38495
尤雨东 <root@ltpp.vip>
2025-02-09 20:53:49 +0800

    feat:sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git init;
 git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/ltpp-code.git;
+git remote add gitee git@gitee.com:ltpp-universe/LTPP-CODE.git;
 git remote add origin git@github.com:ltpp-universe/LTPP-CODE.git;
