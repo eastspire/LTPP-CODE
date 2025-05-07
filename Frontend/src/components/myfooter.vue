5da95df7112b43ecefa30fb5c8d4d15bf5ec63ed
尤雨东 <root@ltpp.vip>
2025-05-07 08:42:37 +0800

    feat: vue

--- Frontend/src/components/myfooter.vue
+++ Frontend/src/components/myfooter.vue
@@ -62,7 +62,7 @@ export default {
         confirmButtonText: '确定',
         cancelButtonText: '重置',
         inputPattern:
-          /^(https?:\/\/(([a-zA-Z0-9]+-?)+[a-zA-Z0-9]+\.)+(([a-zA-Z0-9]+-?)+[a-zA-Z0-9]+))(:\d+)?(\/.*)?(\?.*)?(#.*)?$/,
+          /^(https?:\/\/)((localhost|(\d{1,3}\.){3}\d{1,3}|(([a-zA-Z0-9]+-?)*[a-zA-Z0-9]+\.)+[a-zA-Z]{2,}))(:(\d+))?(\/[^?#]*)?(\?[^#]*)?(#.*)?$/,
         inputErrorMessage: '代理后端地址不正确',
       })
         .then(({ value }) => {
