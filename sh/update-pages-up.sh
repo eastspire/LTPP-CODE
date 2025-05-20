d72f82501fc1275c99464dd648149d57c8d813b6
尤雨东 <root@ltpp.vip>
2025-05-20 13:09:31 +0800

    feat: update

--- sh/update-pages-up.sh
+++ sh/update-pages-up.sh
@@ -7,7 +7,7 @@ cp ../../vercel.json ./
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:ltpp-universe/LTPP-FRONTEND.git master:master
+git push -f git@github.com:eastspire/LTPP-FRONTEND.git master:master
 rm -rf ../frontend
 echo "Press Enter to continue..."
 read -n 1
--- sh/web_build.sh
+++ sh/web_build.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: 18855190718 1491579574@qq.com
  # @Date: 2023-08-22 13:31:27
- # @LastEditors: ltpp-universe 1491579574@qq.com
+ # @LastEditors: eastspire 1491579574@qq.com
  # @LastEditTime: 2023-12-30 19:03:34
  # @FilePath: \LTPP-CODE\web_build.sh
  # @Description: Email:1491579574@qq.com
