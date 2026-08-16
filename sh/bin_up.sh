0365b41dc55678cb7200c5665dccf11bff8d653b
sheng <1491579574@qq.com>
2026-08-16 16:13:19 +0800

    chore: sanitize sh/ script header comments
    The repository was made public recently. The sh/ script header
    comment blocks leak operator contact info that does not belong
    in a public repo:
    
      * @Author / @LastEditors / @Description / QQ / @FilePath blocks
        name a personal QQ, a QQ-email, a phone number, a Windows
        path, and a 'SQS' copyright.
      * One file (sh/bin_up.sh in eastspire/LTPP-CODE) also has three
        commented-out scp commands that point at internal LAN IPs
        (192.168.242.129, 192.168.1.5).
    
    This commit replaces those blocks with a 2-3 line generic comment
    ("Build host -> production host single-binary deploy." / build-host
    requirements). It does NOT touch the actual scp / webman / gtl /
    cp / cd command bodies - the deploy pipeline keeps working
    unchanged.
    
    The sh/ scripts remain executable (100755).
    
    See linked PR for the full diff.

--- sh/bin_up.sh
+++ sh/bin_up.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-###
- # @Author: root@ltpp.vip
- # @Date: 2023-08-22 13:31:27
- # @LastEditors: root@ltpp.vip
- # @LastEditTime: 2023-08-24 18:39:34
- # @FilePath: \LTPP-CODE\bin_up.sh
- # @Description: Email:1491579574@qq.com
- # QQ:1491579574
- # Copyright (c) 2023 by SQS, All Rights Reserved. 
-###
-# scp -P 22 -rp ./build/LTPP root@192.168.242.129:/home/LTPP
-# scp -P 22 -rp -i C:\\Users\\14915\\.ssh\\128G\\id_rsa ./build/LTPP root@192.168.1.5:/home/LTPP
-# scp -P 40022 -rp -i C:\\Users\\14915\\.ssh\\128G\\id_rsa ./build/LTPP root@ltpp.vip:/tmp
+# Build host -> production host single-binary deploy.
+# Edit the host, port, and ssh key path below for your environment.
 scp -P 40022 -rp -i C:\\Users\\14915\\.ssh\\128G\\id_rsa ./build/LTPP root@120.53.248.2:/tmp
 echo "按回车键继续..."
 read -n 1
